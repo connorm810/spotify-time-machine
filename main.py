f70863574d9232e6c104bc453536a7b9c27c936a
Connor J. McEachern <55606915+connorm810@users.noreply.github.com>
2022-11-10 12:45:57 -0500

    Cleaned code, added URI finder function, and updated user input message.

--- main.py
+++ main.py
@@ -18,6 +18,29 @@ def remove_from_list(item: str, rem_list: list):
         rem_list.remove(item)
 
 
+# --- Song URI Finder Function --- #
+def find_songs(song_list: list) -> list:
+    passed = 0
+    skipped = 0
+    song_uris = []
+    for song in tqdm(song_names[3:103], colour="blue", desc="Searching for URIs", leave=False):
+        try:
+            result = sp.search(q=f"artist: {artists_names[song_names[3:103].index(song)]} track: {song}", limit=50)
+            song_uris.append(result["tracks"]["items"][0]["uri"])
+            passed += 1
+        except IndexError:
+            skipped += 1
+        except requests.exceptions.HTTPError:
+            skipped += 1
+        except spotipy.exceptions.SpotifyException:
+            skipped += 1
+
+    print(f"Finished Search | Out of {len(song_names[3:103])} songs, {passed} were found and {skipped} were not.")
+
+    return song_uris
+
+
+# --- Main Function --- #
 def main():
     # --- Spotify Authentication --- #
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET,
@@ -26,8 +49,8 @@ def main():
     user_id = sp.current_user()["id"]
 
     # --- User Input and Web Scraping --- #
-    user_date = input("Which year do you want to travel to? Type the data in this format YYYY-MM-DD: ")
-    # user_date = "2000-08-12"
+    user_date = input("What date do you want to pull music from? (YYYY-MM-DD Format): ")
+    # user_date = "2000-08-12" <-- for debugging
 
     response = requests.get(f"https://www.billboard.com/charts/hot-100/{user_date}/")
     soup = BeautifulSoup(response.text, "html.parser")
@@ -48,22 +71,7 @@ def main():
     print(song_names[3:103])
 
     # --- Finds Song URIs to Allow Addition to Playlist --- #
-    passed = 0
-    skipped = 0
-    song_uris = []
-    for song in tqdm(song_names[3:103], colour="blue", desc="Searching for URIs", leave=False):
-        try:
-            result = sp.search(q=f"artist: {artists_names[song_names[3:103].index(song)]} track: {song}", limit=50)
-            song_uris.append(result["tracks"]["items"][0]["uri"])
-            passed += 1
-        except IndexError:
-            skipped += 1
-        except requests.exceptions.HTTPError:
-            skipped += 1
-        except spotipy.exceptions.SpotifyException:
-            skipped += 1
-
-    print(f"Finished Search | Out of {len(song_names[3:103])} songs, {passed} were found and {skipped} were not.")
+    uri_list = find_songs(song_names)
 
     # --- Add Songs to new Playlist --- #
     print("Adding songs to new playlist...")
